6f92136b9469780dc80da71bdc7129fdcfcc8753
RachelIsrael <racheleisrael4@gmail.com>
2020-03-20 10:47:53 -0400

    Presents one question and evaluates for correct

--- questions.py
+++ questions.py
@@ -21,3 +21,10 @@ question_1 = Question(question="Which of these is a vehicle with three wheels?",
 question_1.save()
 question_2 = Question(question="What word does Aretha Franklin spell out in her iconic 1967 hit?", answer_a="A Respect", answer_b= "B Robocop", answer_c= "C Ridonkulous", correct_answer= "A")
 question_2.save()
+
+print(f"{question_1.question} {question_1.answer_a}, {question_1.answer_b}, or {question_1.answer_c}")
+guess= input("Enter A, B, or C! ")
+if(guess == question_1.correct_answer):
+    print(f"Good job! The correct answer was {question_1.correct_answer}!")
+else:
+    print(f"Not quite! The correct answer was {question_1.correct_answer}.")
