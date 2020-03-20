7964f01440d3cdc80e4eb68d20ae90a5163ac0b7
RachelIsrael <racheleisrael4@gmail.com>
2020-03-20 10:40:54 -0400

    working on seeding w/peewee

--- questions.py
+++ questions.py
@@ -8,4 +8,16 @@ class BaseModel(Model):
 
 class Question(BaseModel):
     question: CharField()
-    answers: 
+    answer_a: CharField()
+    answer_b: CharField()
+    answer_c: CharField()
+    correct_answer: CharField()
+
+db.connect()
+db.drop_tables([Question])
+db.create_tables([Question])
+
+question_1 = Question(question="Which of these is a vehicle with three wheels?", answer_a="A Tricycle", answer_b= "B Trick Daddy", answer_c= "C Triceratops", correct_answer= "A")
+question_1.save()
+question_2 = Question(question="What word does Aretha Franklin spell out in her iconic 1967 hit?", answer_a="A Respect", answer_b= "B Robocop", answer_c= "C Ridonkulous", correct_answer= "A")
+question_2.save()
