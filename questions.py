d0142454b1d6cc7c844edc8189bb1f2b8267bd5e
RachelIsrael <racheleisrael4@gmail.com>
2020-03-21 17:26:50 -0400

    detects end of game, prompts user to play again

--- questions.py
+++ questions.py
@@ -12,7 +12,7 @@ class Question(BaseModel):
     answer_b= CharField()
     answer_c= CharField()
     correct_answer= CharField()
-    card_count= FloatField()
+    card_count= IntegerField()
 
 
 question_data = [{"question": "Which of these is a vehicle with three wheels?", 'answer_a':"A Tricycle", 'answer_b': "B Trick Daddy", 'answer_c': "C Triceratops", 'correct_answer': "A", "card_count": 0}, 
@@ -38,14 +38,20 @@ for list_element in question_data:
 
 def game():
     question_list = Question.select()
+    count = 0
     for game_question in question_list:
         print(f'{game_question.question} {game_question.answer_a}, {game_question.answer_b}, or {game_question.answer_c}')
         guess= input("Enter A, B, or C! ")
         if(guess == game_question.correct_answer):
+            count = count + 1
             print(f"Good job! The correct answer was {game_question.correct_answer}! You have gotten that card correct {game_question.card_count} times")
             game_question.card_count = game_question.card_count + 1
         elif(guess != game_question.correct_answer):
             print(f"Not quite! The correct answer was {game_question.correct_answer}.")
+    if game_question != len(question_list) - 1:
+        play_again = input(f"Good game! You got {count} questions correct! Would you like to play again? y/n ")
+        if play_again == 'y':
+            game()
 
 user = input("To creat a card press C, to Quiz yourself, press G! ")
 if(user == "C"):
